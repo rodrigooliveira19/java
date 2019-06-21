ab69b3ddd244be29d9fbbb8ad88c6b666f1a91c6
rodrigooliveira19 <oliveirarodrigo19@yahoo.com.br>
2019-06-21 16:24:11 -0300

    Alterações no exemplo inner class static

--- generics_colletions/src/main/java/br/com/softblue/java/inner/staticc/ClassOuter.java
+++ generics_colletions/src/main/java/br/com/softblue/java/inner/staticc/ClassOuter.java
@@ -2,12 +2,21 @@ package br.com.softblue.java.inner.staticc;
 
 public class ClassOuter {
 	
+	private static int z = 50; 
+	
+	public static void verificarAcesso() {
+		System.out.println("Tenho acesso");
+	}
+	
 	public static class ClassInner {
 		
 		private int x = 10; 
 		
+		//NÃO ACESSA OS ATRIBUTOS DIRETAMENTE, MAS TEM ACESSO A MÉTODOS E ATRIBUTOS ESTATICOS. 
 		public void imprimir() {
-			System.out.println("Valor "+this.x);
+			System.out.println("Valor "+z);
+			ClassOuter.z+=1;
+			System.out.println("Valor "+z);
 		}
 	}
 
