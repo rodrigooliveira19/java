c38df04e054a90ca05febfd825c916fce3291305
rodrigooliveira19 <oliveirarodrigo19@yahoo.com.br>
2019-06-22 13:24:44 -0300

    Recursos de Lambda com collections

--- generics_colletions/src/main/java/br/com/softblue/java/lambda/Main.java
+++ generics_colletions/src/main/java/br/com/softblue/java/lambda/Main.java
@@ -1,5 +1,9 @@
 package br.com.softblue.java.lambda;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -20,6 +24,25 @@ public class Main {
 		cal.calculate(20);
 		calTwo.calculate(10, 12);
 		System.out.println(calcR.calculate(9));
+		
+		//LAMBDA COM COLLECTIONS
+		List<Integer> list = Arrays.asList(10,1,27,3,4,5,15); 
+		//list.forEach(e->System.out.println(e));
+		
+		list.sort((x,y) -> x.compareTo(y));
+		//list.forEach(e->System.out.println(e));
+		
+		list = new ArrayList<Integer>(); 
+		list.add(2); 
+		list.add(3); 
+		list.add(4); 
+		list.add(5); 
+		list.add(7); 
+		list.add(6); 
+		list.removeIf((e) -> e % 2 != 0); 
+		list.forEach(e->System.out.println(e));
+		
+		
 	}
 
 }
